c08963b5a1807968bc877b963b2140369d38325c
Bryan Luise Reamillo <137306067+Bleu24@users.noreply.github.com>
2025-08-29 08:47:06 +0800

    Refactor localStorage update and add stringification

--- main.js
+++ main.js
@@ -146,6 +146,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const bookId = e.target.closest('.card').id;
             const foundBookIndex = myLibrary.findIndex(book => book.id === bookId);
+            const stringified = JSON.stringify(myLibrary);
 
             if(myLibrary[foundBookIndex].isRead === 'Unread') {
                 myLibrary[foundBookIndex].isRead = 'Reading';
@@ -155,9 +156,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 myLibrary[foundBookIndex].isRead = 'Unread';
             }
 
-            // And here's the code you just wrote to update the DOM and localStorage
+            
             e.target.innerHTML = `Status: ${myLibrary[foundBookIndex].isRead} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-refresh-cw-icon lucide-refresh-cw"><path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/><path d="M21 3v5h-5"/><path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/><path d="M8 16H3v5"/></svg>`;
-            localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+            localStorage.setItem('myLibrary', stringified);
         }
 
     });
